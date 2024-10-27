2c4113eff843c7274c607be52d19e1bf6192d10a
Kai-Cheng Mei <kennymei47@gmail.com>
2024-10-27 13:29:43 -0700

    3rd 10mins

--- main.cpp
+++ main.cpp
@@ -9,19 +9,42 @@
 using namespace std;
 using namespace std::chrono;
 
+
+vector<string> readdata( const string&  "codes.txt") {
+    vector<string> data;
+    ifstream fin( "codes.txt");
+    string line;
+
+    while ( getline(file,line) ) {
+        data.push_back(line);
+    }
+
+    return data;
+}
+
+int mReadV(vecctor<string>& v, const vector<string>& data) {
+    auto start = high_resolution_clock::now()
+    v = data;
+    auto end = high_resolution_clock::now()
+    auto duration = duration_cast<milliseconds>(end - start)
+    return duration.count();
+}
+
+int mReadL(list<string>& l, const vector<string>& data) {
+    auto start = high_resolution_clock::now()
+    
+    auto end = high_resolution_clock::now()
+    auto duration = duration_cast<milliseconds>(end - start)
+    return duration.count();
+}
+
+
 int main() {
     // read data 
-    vector<string> readdata( const string&  "codes.txt") {
-        vector<string> data;
-        ifstream fin( "codes.txt");
-        string line;
-        
-        while ( getline(file,line) ) {
-            data.push_back(line);
-        }
+    
 
-        return data;
-    }
+
+
 
     return 0;
 }
