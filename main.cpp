415c4c73a703a9c307e10556014a5bb3e083f5ab
Kai-Cheng Mei <kennymei47@gmail.com>
2024-10-27 13:40:39 -0700

    4th 10mins

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ using namespace std;
 using namespace std::chrono;
 
 
-vector<string> readdata( const string&  "codes.txt") {
+vector<string> readdata( const string&  codes.txt) {
     vector<string> data;
-    ifstream fin( "codes.txt");
+    ifstream fin("codes.txt");
     string line;
 
     while ( getline(file,line) ) {
@@ -23,25 +23,43 @@ vector<string> readdata( const string&  "codes.txt") {
 }
 
 int mReadV(vecctor<string>& v, const vector<string>& data) {
-    auto start = high_resolution_clock::now()
+    auto start = high_resolution_clock::now();
     v = data;
-    auto end = high_resolution_clock::now()
-    auto duration = duration_cast<milliseconds>(end - start)
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
     return duration.count();
 }
 
 int mReadL(list<string>& l, const vector<string>& data) {
-    auto start = high_resolution_clock::now()
-    
-    auto end = high_resolution_clock::now()
-    auto duration = duration_cast<milliseconds>(end - start)
+    auto start = high_resolution_clock::now();
+    l.assign(data.begin(), data.end());
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    return duration.count();
+}
+
+int mReadS(set<string>& s, const vector<string>& data) {
+    auto start = high_resolution_clock::now();
+    s.insert(data.begin(), data.end());
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
     return duration.count();
 }
 
 
 int main() {
     // read data 
-    
+    vector<string> data = readdata("codes.txt"); 
+    vector<string> v;
+    list<string> l;
+    set<string> s;
+
+
+
+    int readVectorTime = mReadV(v, data);
+    int readListTime = mReadL(l, data);
+    int readSetTime = mReadS(s, data);
+    cout << " Read " << readVectorTime << readListTime << readSetTime << endl;
 
 
 
